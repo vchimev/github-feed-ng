bab6abf016a7c1af97cb9e7bbdfeda93d8a24002
Vasil Chimev <Vasil.Chimev@gmail.com>
2018-09-09 15:04:42 +0300

    feat: display notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NotificationService } from './notification.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'github-feed-ng';
+  notifications = [];
+
+  constructor(private notificationService: NotificationService) {
+    this.notificationService.getNotifications().subscribe((notifications) => {
+      this.notifications = notifications;
+    });
+  }
+
+  getNotificationString(notification) {
+    const { org, repo, title } = notification;
+    return `${org}/${repo}: ${title}`;
+  }
 }
