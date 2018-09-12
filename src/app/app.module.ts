d9311701b924bb36c07e38bd0ac79b47384db522
Vasil Chimev <Vasil.Chimev@gmail.com>
2018-09-12 13:40:13 +0300

    refactor: extract the notification details to a component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NotificationComponent } from './notification/notification.component';
+import { NotificationDetailsComponent } from './notification-details/notification-details.component';
 import { NotificationsListComponent } from './notifications-list/notifications-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NotificationComponent,
-    NotificationsListComponent,
+    NotificationDetailsComponent,
+    NotificationsListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/notification-details/notification-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notification-details',
+  templateUrl: './notification-details.component.html',
+  styleUrls: ['./notification-details.component.css']
+})
+export class NotificationDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
